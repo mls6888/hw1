eeae0a96dd25448978c0d4d5fdfc4615424162d6
mls6888 <mls6888@psu.edu>
2020-09-03 19:15:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+C1 = input("Enter your course 1  letter grade: ")
+char = C1
+CR1 = input("Enter your course 1 credit: ")
+CR1 = int(CR1)
+C2 = input("Enter your course 2 letter grade: ")
+char = C2
+CR2 = input("Enter Course 2 credit: ")
+CR2 = int(CR2)
+C3 = input("Enter Course 3 letter grade: ")
+char = C3
+CR3 = input("Enter Course 3 credit: ")
+CR3 = int(CR3)
+C4 = input("Enter Course 4 letter grade: ")
+char = C4
+CR4 = input("Enter Course 4 credit: ")
+CR4 = int(CR4)
+
+GPA = {'A': 4, 'A-': 3.67, 'B+': 3.33, 'B': 3, 'B-': 2.67, 'C+': 2.33, 'C': 2, 'D': 1, 'F': 0}
+
+
+
+
